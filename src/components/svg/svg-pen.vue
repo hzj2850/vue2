698c214e1f551c9420997ccb4f568db8fae36693
hzj2850 <48989681+hzj2850@users.noreply.github.com>
2023-05-30 09:39:25 +0800

    3

---
+++ src/components/svg/svg-pen.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    props: {
+        bg: {
+            type: String,
+            default: '#ccc'
+        },
+        type: {
+            type: String,
+            default: 'left'
+        }
+    },
+    render() {
+        return <div class="pen-arrow" ref="div">
+            <svg width="100%" height="100%">
+                <polygon points={this.points} style={{fill: this.$props.bg}}/>
+            </svg>
+            <span class={['btn', this.$props.type]} ref="btn">{this.$scopedSlots.default()}</span>
+        </div>
+    },
+    data() {
+        return {
+            points: '',
+        }
+    },
+    mounted() {
+        const el = this.$refs.btn, st = window.getComputedStyle(el),
+              w = parseFloat(st.width),
+              h = parseFloat(st.height);
+        this.$refs.div.style.width = w + "px";
+        this.$refs.div.style.height = h + "px";
+        if(this.$props.type === 'left') this.points = `${h / 2},0 ${w},0 ${w - h / 2},${h / 2} ${w},${h} ${h / 2},${h} 0,${h / 2}`;
+        if(this.$props.type === 'right') this.points = `0,0 ${w - h / 2},0 ${w},${h / 2} ${w - h / 2},${h} 0,${h} ${h / 2},${h / 2}`;
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.pen-arrow{
+    display: inline-block;
+    position: relative;
+    > .btn{
+        display: inline-block;
+        line-height: 45px;
+        padding: 0 35px 0 30px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        font-size: 18px;
+        white-space: nowrap;
+    }
+    > .right{
+        padding: 0 30px 0 35px;
+    }
+}
+</style>
--- src/views/tabs/svg.vue
+++ src/views/tabs/svg.vue
@@ -3,14 +3,18 @@
         <my-btn text="照明总开" type="deongaree"/>
         <my-btn text="照明总关" type="alarm"/>
         <my-btn text="商店" type="success" @callback="$router.push({name: 'shop'})" />
+        <svg-pen type="right">笔箭头</svg-pen>
+        <svg-pen>笔箭头</svg-pen>
     </div>
 </template>
 
 <script>
 import MyBtn from '@/components/svg/my-btn.vue'
+import SvgPen from '@/components/svg/svg-pen.vue'
 export default {
     components: {
         MyBtn,
+        SvgPen
     },
 }
 </script>
@@ -19,4 +23,3 @@ export default {
 .my_btn_1 {
     margin: 30px;
 }
-</style>
